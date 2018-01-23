294a130fe9b54f105f07b8336af8b22171ca0e92
Mateo Burbano <mateoburbano03@gmail.com>
2018-01-23 20:52:11 +0100

    Prueba ajax añadida

---
+++ PruebaAJAX/hacks.js
@@ -0,0 +1,13 @@
+$('button').click(function(e) {
+	$('.contenido').empty();
+	for(var i=0;i<100;i++){
+		llamada();
+	}
+});
+
+function llamada(){
+	$.getJSON('https://dog.ceo/api/breeds/image/random', function(data) {
+		$('.contenido').append('<img class ="foto" src="'+data.message+'">');
+		console.log(data);
+	});
+}

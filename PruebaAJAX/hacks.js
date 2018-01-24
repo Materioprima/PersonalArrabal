0ec801b21bb8b1718b1559bab9eaef6a8c9fad3a
Mateo Burbano <mateoburbano03@gmail.com>
2018-01-24 17:12:45 +0100

    Cambio para no petar servidor con peticiones asinconas a la vez

--- PruebaAJAX/hacks.js
+++ PruebaAJAX/hacks.js
@@ -1,13 +1,14 @@
 $('button').click(function(e) {
 	$('.contenido').empty();
-	for(var i=0;i<100;i++){
-		llamada();
-	}
+	llamada(10);
 });
 
-function llamada(){
-	$.getJSON('https://dog.ceo/api/breeds/image/random', function(data) {
-		$('.contenido').append('<img class ="foto" src="'+data.message+'">');
-		console.log(data);
-	});
+function llamada(cont){
+	if(cont>0){
+		$.getJSON('https://dog.ceo/api/breeds/image/random', function(data) {
+			$('.contenido').append('<img class ="foto" src="'+data.message+'">');
+			console.log(data);
+		});
+		llamada(cont-1);
+	}
 }
